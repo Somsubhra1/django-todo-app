35903d3e8b8dcc755fd9263964f052680c2dac73
Somsubhra Das <das.somsubhra1@gmail.com>
2021-05-15 00:36:14 +0530

    delete create operations done

--- todos/urls.py
+++ todos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ModelListView
+from .views import TodoListView, TodoDeleteView, TodoCreateView
 
 app_name = "todos"
 
 urlpatterns = [
-    path("", ModelListView.as_view(), name="home"),
+    path("", TodoListView.as_view(), name="home"),
+    path("todos/create/", TodoCreateView.as_view(), name="todo-create"),
+    path("todos/<int:pk>/delete", TodoDeleteView.as_view(), name="todo-delete"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DeleteView, CreateView
 from .models import Todo
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+
 
 # Create your views here.
 
 
-class ModelListView(LoginRequiredMixin, ListView):
+class TodoListView(LoginRequiredMixin, ListView):
     model = Todo
     context_object_name = "todos"
 
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user).order_by("-date_created")
+
+
+class TodoDeleteView(LoginRequiredMixin, DeleteView):
+    model = Todo
+
+    success_url = '/'
+
+    def delete(self, request, *args, **kwargs):
+        messages.info(self.request, "Successfully deleted Todo!")
+        return super().delete(request, *args, **kwargs)
+
+
+class TodoCreateView(LoginRequiredMixin, CreateView):
+    model = Todo
+    fields = ["title"]
+
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, "Successfully created Todo!")
+        return super().form_valid(form)
