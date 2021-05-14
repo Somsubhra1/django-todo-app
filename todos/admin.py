1512c9e9f3a2ec70da7a5fb4ca0c89240b886a08
Somsubhra Das <das.somsubhra1@gmail.com>
2021-05-14 13:51:58 +0530

    added todo model

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,5 @@
+from .models import Todo
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Todo)
--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Todo(models.Model):
+
+    title = models.CharField(max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.title} - {self.user.username}'
